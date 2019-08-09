57f0328b3ce9da3d9a26b760d7e8b562be448e2f
Ashwin shrestha <shrashwiniw@gmail.com>
2019-08-09 11:46:13 +0545

    Initial Commit

---
+++ LocationUpdate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationUpdate
+//
+//  Created by Insight Workshop on 8/9/19.
+//  Copyright © 2019 Skyway Innovation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
